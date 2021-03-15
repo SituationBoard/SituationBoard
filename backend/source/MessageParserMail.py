b6f5c5cb0bbf7835894f88a554346a7204de7af0
Daniel Bacher <mail@phill93.de>
2021-03-15 18:43:51 +0100

    first version of the mail plugin

---
+++ backend/source/MessageParserMail.py
@@ -0,0 +1,33 @@
+from os import path
+from typing import Optional
+
+from backend.event.AlarmEvent import AlarmEvent
+from backend.event.SourceEvent import SourceEvent
+from backend.source.MessageParser import MessageParser
+from backend.util.Settings import Settings
+import json
+import re
+
+
+class MessageParserMail(MessageParser):
+
+    def __init__(self, instanceName: str, settings: Settings) -> None:
+        super().__init__("ttp", instanceName, settings)
+        template = open(path.join("misc/mail_templates/", self.getSettingString("template", "") + ".json"), "r").read()
+        self.__template = json.loads(template)
+
+    def parseMessage(self, sourceEvent: SourceEvent, lastEvent: Optional[SourceEvent]) -> Optional[SourceEvent]:
+        alarmEvent = AlarmEvent.fromSourceEvent(sourceEvent)
+        for regex in self.__template['regexes'].items():
+            tmp = ""
+            r = re.search(regex[1], sourceEvent.raw)
+            if r is not None:
+                for value in r.groups():
+                    if tmp == '':
+                        tmp = value
+                    else:
+                        tmp += ' ' + value
+                alarmEvent.__setattr__(regex[0], tmp)
+
+        alarmEvent.flags = AlarmEvent.FLAGS_VALID
+        return alarmEvent
---
+++ backend/source/SourceDriverMail.py
@@ -0,0 +1,60 @@
+import email
+import ssl
+from email.utils import parseaddr
+from email import policy
+from socket import gaierror
+from typing import Optional
+
+from imapclient import IMAPClient
+from imapclient.exceptions import LoginError
+
+from backend.event.SourceEvent import SourceEvent
+from backend.source.MessageParser import MessageParser
+from backend.source.SourceDriver import SourceDriver, SourceState
+from backend.util.Settings import Settings
+
+
+class SourceDriverMail(SourceDriver):
+
+    def __init__(self, instanceName: str, settings: Settings, parser: MessageParser) -> None:
+        super().__init__("mail", instanceName, settings, parser)
+        self.__allowlist = self.getSettingList("allowlist", [])
+        self.__denylist = self.getSettingList("denylist", [])
+        self.__connected = False
+
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)  #Workaround for test
+        context.set_ciphers('DEFAULT@SECLEVEL=1')  #Workaround for test
+        try:
+            self.__imap_server = IMAPClient(self.getSettingString("mail_server", "blubb.test"), use_uid=True,ssl_context=context)
+        except gaierror:
+            self.error("Failed to connect to Mail Server")
+        else:
+            try:
+                self.__imap_server.login(self.getSettingString("mail_user", ""), self.getSettingString("mail_password", ""))
+            except LoginError:
+                self.error("Mail Server login failed")
+            else:
+                self.__connected = True
+                self.__imap_server.select_folder('INBOX',readonly=False)
+
+    def retrieveEvent(self) -> Optional[SourceEvent]:
+        if self.__connected:
+            messages = self.__imap_server.search('UNSEEN')
+            for uid, message_data in self.__imap_server.fetch(messages, "RFC822").items():
+                message = email.message_from_bytes(message_data[b"RFC822"], policy=policy.default)
+                sender = parseaddr(message.get("From"))[1]
+                if self.isSenderAllowed(allowlist=self.__allowlist, denylist=self.__denylist, sender=sender):
+                    sourceEvent = SourceEvent()
+                    sourceEvent.source = SourceEvent.SOURCE_MAIL
+                    sourceEvent.timestamp = message.get('Date')
+                    sourceEvent.sender = sender
+                    sourceEvent.subject = message.get('Subject')
+                    sourceEvent.raw = message.get_body('plain').get_payload()
+                    parsedSourceEvent = self.parser.parseMessage(sourceEvent, None)
+                    return parsedSourceEvent
+
+    def getSourceState(self) -> SourceState:
+        if self.__connected:
+            return SourceState.OK
+        else:
+            return SourceState.ERROR
