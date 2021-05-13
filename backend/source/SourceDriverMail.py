98d6c630ad73f0346a86a49415e50d328976291e
Daniel Bacher <mail@phill93.de>
2021-05-13 16:03:40 +0200

    add exception handling and logging

--- backend/source/SourceDriverMail.py
+++ backend/source/SourceDriverMail.py
@@ -1,8 +1,8 @@
 import email
 import ssl
-from email.utils import parseaddr
 from email import policy
-from socket import gaierror
+from email.utils import parseaddr
+from socket import gaierror, timeout
 from typing import Optional
 
 from imapclient import IMAPClient
@@ -18,27 +18,15 @@ class SourceDriverMail(SourceDriver):
 
     def __init__(self, instanceName: str, settings: Settings, parser: MessageParser) -> None:
         super().__init__("mail", instanceName, settings, parser)
+        self.__settings = settings
         self.__allowlist = self.getSettingList("allowlist", [])
         self.__denylist = self.getSettingList("denylist", [])
-        self.__connected = False
-
-        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)  #Workaround for test
-        context.set_ciphers('DEFAULT@SECLEVEL=1')  #Workaround for test
-        try:
-            self.__imap_server = IMAPClient(self.getSettingString("mail_server", "blubb.test"), use_uid=True,ssl_context=context)
-        except gaierror:
-            self.error("Failed to connect to Mail Server")
-        else:
-            try:
-                self.__imap_server.login(self.getSettingString("mail_user", ""), self.getSettingString("mail_password", ""))
-            except LoginError:
-                self.error("Mail Server login failed")
-            else:
-                self.__connected = True
-                self.__imap_server.select_folder('INBOX',readonly=False)
+        self.__connect()
 
     def retrieveEvent(self) -> Optional[SourceEvent]:
-        if self.__connected:
+        try:
+            if self.isDebug():
+                self.print("Checking for new mails")
             messages = self.__imap_server.search('UNSEEN')
             for _, message_data in self.__imap_server.fetch(messages, "RFC822").items():
                 message = email.message_from_bytes(message_data[b"RFC822"], policy=policy.default)
@@ -51,9 +39,35 @@ class SourceDriverMail(SourceDriver):
                     sourceEvent.raw = message.get_body('plain').get_payload()  # type: ignore[attr-defined]
                     parsedSourceEvent = self.parser.parseMessage(sourceEvent, None)  # type: ignore[union-attr]
                     return parsedSourceEvent
-        return None
+        except (timeout, OSError) as e:
+            self.error("Connection to mailserver timed out")
+            self.__connect()
 
     def getSourceState(self) -> SourceState:
         if self.__connected:
             return SourceState.OK
         return SourceState.ERROR
+
+    def __connect(self):
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)  # Workaround for test
+        context.set_ciphers('DEFAULT@SECLEVEL=1')  # Workaround for test
+        server = self.getSettingString("mail_server", "")
+        user = self.getSettingString("mail_user", "")
+        password = self.getSettingString("mail_password", "")
+        try:
+            if self.isDebug():
+                self.print("Connecting to server {}".format(server))
+            self.__imap_server = IMAPClient(server, use_uid=True, ssl_context=context, timeout=1.0)
+        except gaierror:
+            self.error("Failed to connect to Mail Server")
+        else:
+            try:
+                if self.isDebug():
+                    self.print("Login as user {}".format(user))
+                self.__imap_server.login(user, password)
+            except LoginError:
+                self.error("Mail Server login failed")
+            else:
+                if self.isDebug():
+                    self.print("Login successful")
+                self.__imap_server.select_folder('INBOX', readonly=False)
